4d9a90dcfd5f5a47697b19fc0fdf34fbc91a9cda
rohith <rohithv104@gmail.com>
2025-03-15 19:40:11 +0530

    fixied interceptors

--- client/src/apis/axiosIntance.js
+++ client/src/apis/axiosIntance.js
@@ -2,11 +2,13 @@ import axios from "axios";
 import store from "../store/store";
 import { refreshToken } from "../features/actions/authActions";
 
-
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
+
+let isRefreshing = false;
+let refreshPromise = null;
 
 axiosInstance.interceptors.request.use(
   (config) => {
@@ -27,15 +29,27 @@ axiosInstance.interceptors.response.use(
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
-      try {
-        const newToken = await store.dispatch(refreshToken()).unwrap();
-        localStorage.setItem("accessToken", newToken);
-        originalRequest.headers["Authorization"] = `Bearer ${newToken}`;
-        return axiosInstance(originalRequest);
-      } catch (refreshError) {
-        localStorage.removeItem("accessToken");
-        return Promise.reject(refreshError);
+      if (!isRefreshing) {
+        isRefreshing = true;
+        refreshPromise = store.dispatch(refreshToken()).unwrap();
+
+        try {
+          const newToken = await refreshPromise;
+          localStorage.setItem("accessToken", newToken);
+          axiosInstance.defaults.headers["Authorization"] = `Bearer ${newToken}`;
+        } catch (refreshError) {
+          localStorage.removeItem("accessToken");
+          return Promise.reject(refreshError);
+        } finally {
+          isRefreshing = false;
+          refreshPromise = null;
+        }
+      } else {
+        await refreshPromise;
       }
+
+      originalRequest.headers["Authorization"] = `Bearer ${localStorage.getItem("accessToken")}`;
+      return axiosInstance(originalRequest);
     }
 
     return Promise.reject(error);
